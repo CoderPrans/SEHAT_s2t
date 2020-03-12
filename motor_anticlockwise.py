81a692668415ce7c3f330748db22c99f4ec3f9a0
Pranav Bhaskar <pranavbhaskar17@gmail.com>
2020-03-12 13:04:41 +0000

    moving wheels

---
+++ motor_anticlockwise.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+
+Motor1_1 = 16    # Input Pin
+Motor1_2 = 18    # Input Pin
+Motor1_3 = 22    # Enable Pin
+
+Motor2_1 = 13    # Input Pin
+Motor2_2 = 15    # Input Pin
+Motor2_3 = 11    # Enable Pin
+
+# Motor 1 setup out
+GPIO.setup(Motor1_1,GPIO.OUT)
+GPIO.setup(Motor1_2,GPIO.OUT)
+GPIO.setup(Motor1_3,GPIO.OUT)
+
+# Motor 2 setup out
+GPIO.setup(Motor2_1,GPIO.OUT)
+GPIO.setup(Motor2_2,GPIO.OUT)
+GPIO.setup(Motor2_3,GPIO.OUT)
+
+print("Rotating Antilockwise")
+GPIO.output(Motor2_1,GPIO.LOW)
+GPIO.output(Motor2_2,GPIO.HIGH)
+GPIO.output(Motor2_3,GPIO.HIGH)
+
+GPIO.output(Motor1_1,GPIO.HIGH)
+GPIO.output(Motor1_2,GPIO.LOW)
+GPIO.output(Motor1_3,GPIO.HIGH)
+
+sleep(3)
+
+print("STOP")
+GPIO.output(Motor1_3,GPIO.LOW)
+GPIO.output(Motor2_3,GPIO.LOW)
+
+GPIO.cleanup()
--- record.sh
+++ record.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-arecord -D hw:1,0 -d 5 -f cd test.wav -c 2
+arecord -D hw:1,0 -d 5 -f cd test.wav -c 1
 wait
 aplay test.wav
 
@@ -13,7 +13,7 @@ KEY=$(cat apiKey)
 RESULTS=$(curl -X POST -u "apikey:$KEY" \
    --header "Content-Type: audio/wav" \
    --data-binary @test.wav \
-  "https://gateway-lon.watsonplatform.net/speech-to-text/api/v1/recognize" | \
+   "https://gateway-lon.watsonplatform.net/speech-to-text/api/v1/recognize" | \
 jq -r '.results[]') 
 
 echo $RESULTS | jq -r '.alternatives[]'
@@ -22,10 +22,22 @@ TRANSCRIPT=$(echo $RESULTS | jq -r '.alternatives[].transcript')
 
 for word in $TRANSCRIPT
 do
-    if [ ${#word} -gt 3 ]; then
-        echo "$word :: "$(ag -i $word commands.txt) >> temp 
+    #if [ ${#word} -gt 3 ]; then
+    #    echo "$word :: "$(ag -i $word commands.txt) >> temp 
+    if [ $word == "forward" ]; then
+	echo "going forward"
+	python3 motor_forward.py
+    elif [ $word == "backward" ]; then
+	echo "going backward"
+	python3 motor_backward.py
+    elif [ $word == "turn right" ]; then
+	echo "turning right"
+	python3 motor_clockwise.py
+    elif [ $word == "turn left" ]; then
+	echo "turning left"
+	python3 motor_anticlockwise.py
     fi
 done
 
-cat temp
-rm temp
+#cat temp
+#rm temp
